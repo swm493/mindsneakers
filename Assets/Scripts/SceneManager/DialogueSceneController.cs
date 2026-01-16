0583ec966d5139e71ba29bb77a1a7942069e7c84
swm493 <seungjaeha0903@gmail.com>
2026-01-16 22:28:02 +0900

    [Fix] Background and font

--- Assets/Scripts/SceneManager/DialogueSceneController.cs
+++ Assets/Scripts/SceneManager/DialogueSceneController.cs
@@ -1,11 +1,15 @@
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.UI;
 
 public class DialogueSceneController : MonoBehaviour
 {
     [Header("Events")]
     [SerializeField] private UnityEvent onDialogueSequenceStarted;
     [SerializeField] private UnityEvent onDialogueSequenceEnded;
+
+    [Header("UI")]
+    [SerializeField] private Image background;
 
     private void Start()
     {
@@ -49,5 +53,30 @@ public class DialogueSceneController : MonoBehaviour
             InputManager.Instance.EnableInput();
         else
             InputManager.Instance.DisableInput();
+    }
+
+    public void SetBackground()
+    {
+        switch (SaveManager.Instance.playerData.level)
+        {
+            case 0:
+                background.sprite = Resources.Load<Sprite>("Backgrounds/LivingRoom");
+                break;
+            case 1:
+                background.sprite = Resources.Load<Sprite>("Backgrounds/Kitchen");
+                break;
+            case 2:
+                background.sprite = Resources.Load<Sprite>("Backgrounds/LivingRoom");
+                break;
+            case 6:
+                background.sprite = Resources.Load<Sprite>("Backgrounds/LivingRoom");
+                break;
+            case 7:
+                background.sprite = Resources.Load<Sprite>("Backgrounds/LivingRoom");
+                break;
+            default:
+                MyDebug.Log("No background set for this level.");
+                break;
+        }
     }
 }
